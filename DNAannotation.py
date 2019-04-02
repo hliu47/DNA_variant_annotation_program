97365cc4f22701b3c58fec0f9890ba4d7f152ef6
Haipeng Liu <hliu47@uic.edu>
2019-04-02 14:34:29 -0500

    Update DNAannotation.py

--- DNAannotation.py
+++ DNAannotation.py
@@ -289,7 +289,7 @@ ref_genome = args.genome                        # Reference genome, obtained fro
 anno_order_file = 'ann_deleterious_order.txt'   # File that contains orders of variant deleteriousness according to Sequancy Ontology.
 output_file = 'DNA_annotation.vcf'              # Annotated VCF output from this program
 
-# Remove any previous .tmp 
+# Remove any previous .temp 
 if os.path.isfile("temp_out_vcf.temp")
     subprocess.run(['rm', 'temp_out_vcf.temp'])
 if os.path.isfile("pickle_dump.temp")
